3d96cbe2afaccb1223362d96e1e3125f09798feb
yopico777 <45596398+yopico777@users.noreply.github.com>
2026-09-17 14:15:24 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const C='tehonbiki-v07';
+const A=['./','./index.html','./manifest.webmanifest'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(A)))});
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
